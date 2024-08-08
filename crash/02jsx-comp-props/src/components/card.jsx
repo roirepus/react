7792bed87eab42a983882ba4978ee5c96244f5aa
roirepus <rajsinsinwar159@gmail.com>
2024-08-08 19:39:41 +0530

    hooks

---
+++ crash/02jsx-comp-props/src/components/card.jsx
@@ -0,0 +1,6 @@
+function Card (props){
+		return <>
+				<h1>this is  component{props.count}</h1>
+		</>
+}
+export default Card
---
+++ crash/06lifecycle-useEffect/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
