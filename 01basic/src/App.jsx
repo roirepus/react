2a1717e2239a2c1352d4b8c0313f5e5d676c97f5
roirepus <rajsinsinwar159@gmail.com>
2024-08-16 06:21:38 +0530

    hooks

---
+++ 01basic/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  let x=5;
+  let y=++x;
+
+  return (
+    <>
+      <h1>{y}</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import Card from './components/card.jsx'
+function App() {
+  return (
+              <>
+      <h1
+        className="text-white mb-4 bg-purple-500">tailwind test</h1>
+      <Card username='hemlo'/>
+
+    </>
+
+  );
+}
+
+export default App;
